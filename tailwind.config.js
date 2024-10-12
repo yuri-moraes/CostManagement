371a2c80da5c6e1f41ea1a27ddc7c33a76d338cb
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-10-12 14:24:40 -0300

    Fix build scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./**/*.html"],
+  content: [
+    "./src/**/*.{html,js,ts}",
+    "./index.html", // Certifique-se de apontar corretamente para seus arquivos HTML
+    "!./node_modules/**",  // Exclui o diretório node_modules
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
