6d3ca8271b7cdc00f7eb13a0c431ebb84c172463
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-10-14 09:46:11 -0300

    added node version in engines

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: [
-    "./src/**/*.{html,js,ts}",
-    "./index.html", // Certifique-se de apontar corretamente para seus arquivos HTML
-    "!./node_modules/**",  // Exclui o diretório node_modules
-  ],
+  content: ["./src/**/*.{html,js,ts}", "./index.html", "!./node_modules/**"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
